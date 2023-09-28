e38e28f946a7ea7627fa47bf9ac46d296584e071
Ashutosh Sharma <asharma89505@gmail.com>
2023-09-28 12:58:33 +0530

    changes done

--- cwaserver/index.js
+++ cwaserver/index.js
@@ -6,16 +6,16 @@ const blogRoute = require('./routes/blogRoute')
 const adminRoute = require('./routes/adminRoute')
 const cwaRoute = require('./routes/cwaRoute')
 const config = require('./config/config')
+const axios = require('axios')
 try {
-    // mongoose.connect('mongodb://admin:Dev.ash%4073@codewithash.blog:27017/CWA?authSource=admin')
-    mongoose.connect(`mongodb://${config.dbUsername}:${config.dbPass}@${config.dbHost}:27017/CWA?authSource=admin`)
-
+    // mongoose.connect(`mongodb://${config.dbUsername}:${config.dbPass}@${config.dbHost}:27017/CWA?authSource=admin`)
+    mongoose.connect(`mongodb://admin:Dev.ash%4073@43.205.240.22:27017/codewithash?authSource=admin`)
 } catch (error) {
     console.log(error.message);
 }
 
 app.use(express.static('public'))
-
+app.use(express.json())
 app.set('view engine','ejs')
 app.set('views', './views')
 
@@ -23,6 +23,11 @@ app.use('/',blogRoute)
 app.use('/',adminRoute)
 app.use('/',cwaRoute)
 
+app.get('/animal',async (_,res)=>{
+    const data = await axios.get('https://www.codewithash.blog/blog/search/node')
+    console.log(data);
+    res.send(data.s)
+})
 app.get('*',(_,res)=>{
     res.render('404')
 })
